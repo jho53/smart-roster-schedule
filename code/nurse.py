5788c2c1c183efd6aa79507e86e48da278c01b24
Jaguar Perlas <jperlas@my.bcit.ca>
2020-10-02 12:34:12 -0700

    get_metadata() for nurse and patient

--- code/nurse.py
+++ code/nurse.py
@@ -11,3 +11,18 @@ class Nurse(Base):
     last_name = Column(String(250), nullable=False)
     pod = Column(String(100), nullable=True)
     bed = Column(Integer, nullable=True)
+
+    def __init__(self, id, first_name, last_name, pod, bed):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.pod = pod
+        self.bed = bed
+
+    def get_metadata(self) -> dict:
+        nurse_dict = {'id': self.id,
+                      'first_name': self.first_name,
+                      'last_name': self.last_name,
+                      'pod': self.pod,
+                      'bed': self.bed}
+        return nurse_dict
