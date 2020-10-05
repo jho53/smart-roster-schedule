bf31e4334e350c83fd6cb8f3594eb85ec123f9a2
miguelcapaz7 <55458822+miguelcapaz7@users.noreply.github.com>
2020-10-04 19:23:02 -0700

    Added PyMongo module

--- code/main.py
+++ code/main.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__, instance_relative_config=True)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/smartroster"
+mongo = PyMongo(app)
 
 
 if __name__ == "__main__":
-    app.run
+    app.run()
