24237885eb46bc849b92c36c791380f76908d04a
miguelcapaz7 <55458822+miguelcapaz7@users.noreply.github.com>
2020-10-10 17:10:56 -0700

    Added login and logout routes

--- code/main.py
+++ code/main.py
@@ -1,9 +1,49 @@
-from flask import Flask
-from flask_pymongo import PyMongo
+from flask import Flask, render_template, redirect, url_for, request, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__, instance_relative_config=True)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/smartroster"
-mongo = PyMongo(app)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute(
+            'SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,)
+        )
+
+        account = cursor.fetchone()
+
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            return 'Logged in successfully!'
+        else:
+            msg = 'Incorrect username/password!'
+    return render_template('index.html', msg=msg)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 
 if __name__ == "__main__":
