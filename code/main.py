bee8188af2de6a103328f667b2169cbd97ddc9c1
Jimmy Ho <jimmy0_@hotmail.com>
2020-10-13 16:09:36 -0700

    created preliminary flask templates

--- code/main.py
+++ code/main.py
@@ -36,6 +36,7 @@ def login_user():
         )
         session['username'] = db.user
         session['loggedin'] = True
+        print(session, file=sys.stderr)
         return render_template("index.html")
 
     except Exception:
