b5fb0f2d228003848ef965b8aa491a264e5baca2
Jimmy Ho <jim.eho97@gmail.com>
2020-11-05 23:03:00 -0800

    thicc update

--- code/main.py
+++ code/main.py
@@ -164,7 +164,7 @@ def nurse_records():
 
 @app.route("/addNurseRecords", methods=["POST"])
 def add_nurse_records():
-        
+
     nurse_name = request.form['create_nurse_name']
     nurse_area = request.form['create_nurse_area']
     nurse_rotation = request.form['create_nurse_rotation']
@@ -196,10 +196,8 @@ def add_nurse_records():
         print(error)
 
 
-
 @app.route("/editNurseRecords", methods=["POST"])
 def edit_nurse_records():
-
 
     nurse_id = request.form['edit_nurse_id']
     nurse_name = request.form['edit_nurse_name']
@@ -222,7 +220,6 @@ def edit_nurse_records():
     arguments = (nurse_name, nurse_area, nurse_rotation, nurse_group,
                  nurse_fte, nurse_skill, nurse_a_trained, nurse_transfer, nurse_iv, nurse_adv_role, nurse_prev_pat, nurse_DTA, nurse_comments, nurse_id)
 
-
     try:
         cursor.execute(query, arguments)
         db.commit()
@@ -232,8 +229,6 @@ def edit_nurse_records():
 
     except Exception as error:
         print(error)
-
-
 
 
 @app.route("/deleteNurseRecords", methods=["POST"])
@@ -284,8 +279,8 @@ def add_patient_records():
     query = "insert into smartroster.patients(name, clinical_area, bed_num, acuity, a_trained, transfer, iv, one_to_one, previous_nurses, admission_date, comments )" \
         "VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 
-    arguments = (patient_name, patient_clinical_area, patient_bed, patient_acuity, 
-                    patient_a_trained, patient_transfer, patient_iv, patient_one_to_one, patient_previous_nurses, patient_date_admitted ,patient_comments)
+    arguments = (patient_name, patient_clinical_area, patient_bed, patient_acuity,
+                 patient_a_trained, patient_transfer, patient_iv, patient_one_to_one, patient_previous_nurses, patient_date_admitted, patient_comments)
 
     try:
         cursor.execute(query, arguments)
@@ -295,10 +290,9 @@ def add_patient_records():
         cursor.execute("SELECT * FROM patients")
         patient_list = cursor.fetchall()
         return render_template("./Records/patientRecord.html", loggedin=session['loggedin'], patientList=patient_list, patientHeaders=PATIENT_HEADERS)
-        
+
     except Exception as error:
         print(error)
-
 
 
 @app.route("/editPatientRecords", methods=["POST"])
@@ -323,20 +317,18 @@ def edit_patient_records():
         " transfer = %s, iv = %s, one_to_one = %s, previous_nurses = %s, admission_date = %s, discharged_date = %s, comments = %s WHERE id = %s"
 
     arguments = (patient_name, patient_clinical_area, patient_bed, patient_acuity, patient_a_trained, patient_transfer,
-                    patient_iv, patient_one_to_one, patient_previous_nurses, patient_date_admitted, patient_date_discharged, patient_comments, patientid)
+                 patient_iv, patient_one_to_one, patient_previous_nurses, patient_date_admitted, patient_date_discharged, patient_comments, patientid)
 
     try:
         cursor.execute(query, arguments)
         db.commit()
-            # Grabs all patients
+        # Grabs all patients
         cursor.execute("SELECT * FROM patients")
         patient_list = cursor.fetchall()
         return render_template("./Records/patientRecord.html", loggedin=session['loggedin'], patientList=patient_list, patientHeaders=PATIENT_HEADERS)
 
     except Exception as error:
         print(error)
-    
-
 
 
 @app.route("/deletePatientRecords", methods=["POST"])
@@ -351,15 +343,13 @@ def delete_patient_records():
         cursor.execute(query)
         db.commit()
         # Grabs all patients
-        
+
         cursor.execute("SELECT * FROM patients")
         patient_list = cursor.fetchall()
         return render_template("./Records/patientRecord.html", loggedin=session['loggedin'], patientList=patient_list, patientHeaders=PATIENT_HEADERS)
 
     except Exception as error:
         print(error)
-
-
 
 
 @app.route("/patientRecordsSubmit", methods=['POST'])
@@ -391,12 +381,19 @@ def settings():
 
 @app.route("/currentCAASheet")
 def current_CAASheet():
+
     return render_template("./Assignment Sheets/cur_caaSheet.html", loggedin=session['loggedin'])
 
 
 @app.route("/currentPNSheet")
 def current_PNSheet():
-    return render_template("./Assignment Sheets/cur_pnSheet.html", loggedin=session['loggedin'])
+    # Grab nurse and patient tables
+    cursor.execute("SELECT * FROM nurses WHERE current_shift=1")
+    nurse_list = cursor.fetchall()
+    cursor.execute("SELECT * FROM patients")
+    patient_list = cursor.fetchall()
+
+    return render_template("./Assignment Sheets/cur_pnSheet.html", loggedin=session['loggedin'], nurseList=nurse_list, patientList=patient_list)
 
 
 @app.route("/pastCAASheet")
@@ -418,14 +415,15 @@ def assign_nurse_patient() -> dict:
     # Grab Patients
     patients = []
 
-    cursor.execute('SELECT * FROM patients WHERE discharged_date="-" ORDER BY acuity DESC, a_trained DESC, transfer DESC, iv DESC;')
+    cursor.execute(
+        'SELECT * FROM patients WHERE discharged_date="-" ORDER BY acuity DESC, a_trained DESC, transfer DESC, iv DESC;')
     patient_list = cursor.fetchall()
 
     for row in patient_list:
         x = Patient(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11],
-                         row[12], row[13], row[14])
+                    row[12], row[13], row[14])
         patients.append(x)
-    
+
     print(patients)
 
     # Grab Nurses
@@ -436,10 +434,10 @@ def assign_nurse_patient() -> dict:
 
     for row in nurse_list:
         x = Nurse(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11],
-                         row[12], row[13], row[14], row[15], row[16], row[17], row[18])
+                  row[12], row[13], row[14], row[15], row[16], row[17], row[18])
         nurses.append(x)
 
-        assignments[row[0]] = { 'num_patients': 0, 'patients': [], 'prev_p': [] }
+        assignments[row[0]] = {'num_patients': 0, 'patients': [], 'prev_p': []}
 
     print(nurses)
 
@@ -450,7 +448,8 @@ def assign_nurse_patient() -> dict:
         print(prev_p)
 
         if prev_p != "[]":
-            prev_p_list = prev_p.split(",") # assume that prev_p is a comma separated list of patient IDs
+            # assume that prev_p is a comma separated list of patient IDs
+            prev_p_list = prev_p.split(",")
 
             for p in patients:
                 if p.get_id() == prev_p_list[-1]:
@@ -507,7 +506,6 @@ def assign_nurse_patient() -> dict:
                 if len(eligible_nurse_objects) < 1:
                     i += 1
 
-
             nurse_weights = {}
             max_points = 0
 
@@ -526,7 +524,7 @@ def assign_nurse_patient() -> dict:
                 # if nurse matches priority, give nurse 2 points
                 if eno.get_priority() == 1:
                     nurse_weights[eno.get_id()] += 2
-                
+
                 if nurse_weights[eno.get_id()] > max_points:
                     max_points = nurse_weights[eno.get_id()]
 
@@ -535,9 +533,10 @@ def assign_nurse_patient() -> dict:
             for eno in eligible_nurse_objects:
                 if nurse_weights[eno.get_id()] == max_points:
                     eligible_max_nurses.append(eno.get_id())
-            
+
             # algorithm that matches nurse to patient starting from lowest skill level
-            sorted_eligible_nurses = sorted(eligible_nurse_objects, key=lambda x: x.skill_level, reverse=False)
+            sorted_eligible_nurses = sorted(
+                eligible_nurse_objects, key=lambda x: x.skill_level, reverse=False)
 
             for sen in sorted_eligible_nurses:
                 if sen.get_id() in eligible_max_nurses:
